d7eead5d93f8c216d1a14b0246ad97c80152b5fd
Vlademir Junior <vlademirjunior@protonmail.com>
2024-07-07 18:12:15 -0300

    feat(Environment): :sparkles: Add HTTPS
    Added use https redirection and hsts
    
    SSL

--- ToDoApp/FrontEnd/Program.cs
+++ ToDoApp/FrontEnd/Program.cs
@@ -11,7 +11,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
